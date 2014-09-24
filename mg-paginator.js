112bdd6071d47395ea24c9ae20a36f64f1a1d0b8
Michael <mbg@outlook.com>
2014-09-24 17:01:28 +0100

    Update mg-paginator.js

--- mg-paginator.js
+++ mg-paginator.js
@@ -1,52 +1,71 @@
+angular.module('app')
+.directive('mgPaginator', function($compile) {
+    return {
+        templateUrl: "/views/paginator/paginator.html",
+        replace: true,
+        transclude: true,
+        link: function(scope, element, attrs, ctrl, transclude) {
+            var $ = scope;
+            $.$watch('paginatedMarks', function(newVal) {
+                if (!newVal) {
+                    return;
+                }
+                $.groupOfArrays = [];
 
-  
-  .directive('mgPaginator', function() {
-      return {
-            templateUrl: "sample-partial.html",
-            link: function(scope, elements, attrs){
-                 scope.$watch('groupedArrayOfItems', function(newVal){
-                      if (newVal === undefined){
-                          return;
-                      }
-                      for (var i=0; i < newVal.length; i+= scope.itemsPerPage) {
-                           var slice = newVal.slice(i, i+ scope.itemsPerPage);
-                           scope.groupOfArrays.push(slice);
-                      }
-                      scope.pageNumber = 1;
-                 })
+                for (var i = 0; i < newVal.length; i += $.itemsPerPage) {
+                    var slice = newVal.slice(i, i + $.itemsPerPage);
+                    $.groupOfArrays.push(slice);
+                }
+                $.items = $.groupOfArrays[0];
+                $.pageNumber = 1;
+            })
 
-                  scope.$watch('pageNumber', function(newPage){
-                      scope.items = scope.groupOfArrays[newPage -1];
-                  })
-    
-            },
-            scope: {
-                groupedArrayOfItems: "=",
-                itemsPerPage: "="
-            },
-            controller: function($scope) {
-                $scope.groupOfArrays = [];
-                $scope.prevPage = function(pageNumber){
-                    $scope.pageNumber--;
-                };
-                $scope.nextPage = function(pageNumber){
-                    $scope.pageNumber++;
-                };
-                $scope.firstPage = function(){
-                    $scope.pageNumber = 1;
-                };
-                $scope.lastPage = function(){
-                    $scope.pageNumber = $scope.groupOfArrays.length;
-                };
-                $scope.checkIfFirst = function(pageNumber){
-                    if (pageNumber === 1){
-                      return true;
-                    }
-                };
-                $scope.checkIfLast = function(pageNumber){
-                   if (pageNumber === $scope.groupOfArrays.length){
-                      return true;
-                   }
-                };
-          }
-      })
+            $.$watch('pageNumber', function(newPage) {
+                $.items = $.groupOfArrays[newPage - 1];
+            });
+            // transclude function gives directive scope to transcluded content, rather
+            // than default position of transcluded content using controller scope 
+            transclude($, function(clone, $){
+                element.prepend(clone);
+                //childElement = angular.element('<div></div>')
+                //childElement.append(clone);
+                //childElement.append($compile(clone)($));
+            });
+        },
+        scope: {
+            paginatedMarks: '=',
+            itemsPerPage: '=',
+            showModal: '&'
+        },
+        controller: function($scope) {
+            var $ = $scope;
+            $.groupOfArrays = [];
+            $.prevPage = function() {
+                $.pageNumber--;
+            };
+            $.nextPage = function() {
+                $.pageNumber++;
+            };
+            $.firstPage = function() {
+                $.pageNumber = 1;
+            };
+            $.lastPage = function() {
+                $.pageNumber = $scope.groupOfArrays.length;
+            };
+            $.checkIfFirst = function(pageNumber) {
+                if (pageNumber === 1) {
+                    return true;
+                }
+            };
+            $.checkIfLast = function(pageNumber) {
+                if (pageNumber === $scope.groupOfArrays.length) {
+                    return true;
+                }
+            };
+            $.outerShowModal = function(tm) {
+                var func = $scope.showModal();
+                func(tm);
+            }
+        }
+    }
+})
